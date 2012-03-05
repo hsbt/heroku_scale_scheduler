cfc0449d6ba0a2f3cc0a8a5ff69ffbeca3517f8c
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-05 19:34:15 +0900

    support cedar stack

--- lib/heroku_scale_scheduler/client.rb
+++ lib/heroku_scale_scheduler/client.rb
@@ -2,19 +2,21 @@ require 'heroku/client'
 
 module HerokuScaleScheduler
   class Client
-    def initialize(type)
-      @type = type
-    end
-
-    def run
+    def run(type = :ps)
       client = Heroku::Client.new(*Heroku::Auth.read_credentials)
       config = HerokuScaleScheduler::Config.read_config
       run_at = (Time.now.strftime('%H%M').to_i / 10) * 10
 
-      if qty = config[@type.to_s][run_at]
-        case @type
+      if type != :ps && qty = config[type.to_s][run_at]
+        case type
         when :dynos then client.set_dynos(config['app'], qty)
         when :workers then client.set_workers(config['app'], qty)
+        end
+      else
+        config.keys.each do |process|
+          if qty = config[process][run_at]
+            client.ps_scale(config['app'], :type => process, :qty => qty)
+          end
         end
       end
     end
--- lib/heroku_scale_scheduler/tasks/heroku_scale_scheduler.rake
+++ lib/heroku_scale_scheduler/tasks/heroku_scale_scheduler.rake
@@ -2,12 +2,17 @@ namespace :heroku do
   namespace :scale do
     desc 'scaling heroku dynos'
     task :dynos do
-      HerokuScaleScheduler::Client.new(:dynos).run
+      HerokuScaleScheduler::Client.new.run(:dynos)
     end
 
     desc 'scaling heroku workers'
     task :workers do
-      HerokuScaleScheduler::Client.new(:workers).run
+      HerokuScaleScheduler::Client.new.run(:workers)
+    end
+
+    desc 'scaling heroku process'
+    task :process do
+      HerokuScaleScheduler::Client.new.run
     end
   end
 end
