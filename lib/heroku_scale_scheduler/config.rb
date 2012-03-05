3cb2a9b0f4a41f4317b8f0663355ec1c198fc6ab
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-05 19:34:01 +0900

    bugfix YAML.load needs string... not a IO object.

--- lib/heroku_scale_scheduler/config.rb
+++ lib/heroku_scale_scheduler/config.rb
@@ -2,7 +2,7 @@ module HerokuScaleScheduler
   class Config
     def self.read_config
       if File.exists? Rails.root.join('config', 'heroku_ps.yml')
-        YAML.load(Rails.root.join('config', 'heroku_ps.yml'))
+        YAML.load(File.read(Rails.root.join('config', 'heroku_ps.yml')))
       else
         {}
       end
