7f35a544e31f1c1223d062c781afd9991bd4dd94
SHIBATA Hiroshi <shibata.hiroshi@gmail.com>
2012-03-08 19:52:29 +0900

    support multi-application

--- lib/heroku_scale_scheduler/client.rb
+++ lib/heroku_scale_scheduler/client.rb
@@ -7,15 +7,17 @@ module HerokuScaleScheduler
       config = HerokuScaleScheduler::Config.read_config
       run_at = (Time.now.strftime('%H%M').to_i / 10) * 10
 
-      if type != :ps && qty = config[type.to_s][run_at]
-        case type
-        when :dynos then client.set_dynos(config['app'], qty)
-        when :workers then client.set_workers(config['app'], qty)
-        end
-      else
-        config.keys.each do |ps|
-          if qty = config[ps][run_at]
-            client.ps_scale(config['app'], :type => ps, :qty => qty)
+      config.keys.each do |app|
+        if type != :ps && qty = config[app][type.to_s][run_at]
+          case type
+          when :dynos then client.set_dynos(app, qty)
+          when :workers then client.set_workers(app, qty)
+          end
+        else
+          config[app].keys.each do |ps|
+            if qty = config[app][ps][run_at]
+              client.ps_scale(app, :type => ps, :qty => qty)
+            end
           end
         end
       end
